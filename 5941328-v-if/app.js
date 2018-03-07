8280ada69caa8cf246c56e2771293fa25cc0e543
David Shaal <david.shaal@gmail.com>
2018-03-07 16:51:00 -0500

    before For Exercise -

---
+++ 5941328-v-if/app.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+
+    new Vue({
+        el: '#app',
+        data: {
+            ingredients: ['meat', 'fruit', 'cookies'],
+            persons: [
+                {name: 'Max', age: 27, color: 'red'},
+                {name: 'Anna', age: 'unknown', color: 'blue'}
+            ]
+        },
+    });
+}
--- Exercise 4/app.js
+++ Exercise 4/app.js
@@ -2,10 +2,27 @@ window.onload = function () {
 
     new Vue({
         el: '#exercise',
-        data: {},
-        methods: {
-            startEffect: function () {
+        data: {
+            effectClass: 'hello'
+        },
+        watch: {
+            effectClass: function() {
+                setTimeout(function () {
+                    return this.startEffect();
+                },500)
+            }
+        },
 
+        methods: {
+            startEffect: function() {
+                if (this.effectClass == 'highlight') {
+                    this.effectClass = 'shrink';
+                } else {
+                    this.effectClass = 'highlight';
+                }
+            },
+            startProgress: function () {
+                
             }
         }
     });
